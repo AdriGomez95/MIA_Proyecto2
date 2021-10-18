dc67eff9112fc6dd68ef5bc9a953c47252fe8fe2
adriana <adriana.d@hotmail.com>
2021-10-18 13:17:39 -0600

    pequeño cambio

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -6,9 +6,18 @@ function App() {
   return (
     <div className="App">
     <h2>Hola mundo</h2>
+    <button onClick={consuta}>prueba</button>
     <Pag1/>
     </div>
   );
 }
+
+
+const consuta = () =>{
+  console.log("peticion realizada");
+}
+
+
+
 
 export default App;
